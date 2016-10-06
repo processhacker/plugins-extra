5297e676002a9371f14de6683e2a7149ee590a63
dmex <dmex04@gmail.com>
2016-10-07 08:38:15 +1100

    Initial ObjectManagerPlugin commit

---
+++ ObjectManagerPlugin/main.c
@@ -0,0 +1,151 @@
+/*
+ * Process Hacker Extra Plugins -
+ *   Object Manager Plugin
+ *
+ * Copyright (C) 2016 dmex
+ *
+ * This file is part of Process Hacker.
+ *
+ * Process Hacker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Process Hacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "main.h"
+
+PPH_PLUGIN PluginInstance;
+static PH_CALLBACK_REGISTRATION PluginLoadCallbackRegistration;
+static PH_CALLBACK_REGISTRATION PluginShowOptionsCallbackRegistration;
+static PH_CALLBACK_REGISTRATION PluginMenuItemCallbackRegistration;
+static PH_CALLBACK_REGISTRATION MainMenuInitializingCallbackRegistration;
+
+VOID LoadCallback(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
+    )
+{
+    NOTHING;
+}
+
+VOID ShowOptionsCallback(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
+    )
+{
+    NOTHING;
+}
+
+VOID MenuItemCallback(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
+    )
+{
+    PPH_PLUGIN_MENU_ITEM menuItem = (PPH_PLUGIN_MENU_ITEM)Parameter;
+
+    if (menuItem != NULL)
+    {
+        switch (menuItem->Id)
+        {
+        case WINOBJ_MENU_ITEM:
+            {
+                 DialogBox(
+                     PluginInstance->DllBase,
+                     MAKEINTRESOURCE(IDD_OBJMGR),
+                     NULL,
+                     WinObjDlgProc
+                     );
+            }
+            break;
+        }
+    }
+}
+
+VOID NTAPI MainMenuInitializingCallback(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
+    )
+{
+    PPH_PLUGIN_MENU_INFORMATION menuInfo = Parameter;
+    PPH_EMENU_ITEM systemMenu;
+
+    if (menuInfo->u.MainMenu.SubMenuIndex != PH_MENU_ITEM_LOCATION_TOOLS)
+        return;
+
+    if (!(systemMenu = PhFindEMenuItem(menuInfo->Menu, 0, L"System", 0)))
+    {
+        PhInsertEMenuItem(menuInfo->Menu, PhPluginCreateEMenuItem(PluginInstance, PH_EMENU_SEPARATOR, 0, L"", NULL), -1);
+        PhInsertEMenuItem(menuInfo->Menu, systemMenu = PhPluginCreateEMenuItem(PluginInstance, 0, 0, L"System", NULL), -1);
+    }
+
+    PhInsertEMenuItem(systemMenu, PhPluginCreateEMenuItem(PluginInstance, 0, WINOBJ_MENU_ITEM, L"Object Manager", NULL), -1);
+}
+
+LOGICAL DllMain(
+    _In_ HINSTANCE Instance,
+    _In_ ULONG Reason,
+    _Reserved_ PVOID Reserved
+    )
+{
+    switch (Reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        {
+            PPH_PLUGIN_INFORMATION info;
+            PH_SETTING_CREATE settings[] =
+            {
+                { IntegerPairSettingType, SETTING_NAME_WINDOW_POSITION, L"100,100" },
+                { ScalableIntegerPairSettingType, SETTING_NAME_WINDOW_SIZE, L"@96|520,290" },
+                { StringSettingType, SETTING_NAME_COLUMNS, L"" }
+            };
+
+            PluginInstance = PhRegisterPlugin(PLUGIN_NAME, Instance, &info);
+
+            if (!PluginInstance)
+                return FALSE;
+
+            info->DisplayName = L"Object Manager";
+            info->Author = L"dmex";
+            info->Description = L"Plugin for viewing the NT Object Manager via the Tools menu";
+            info->HasOptions = FALSE;
+
+            PhRegisterCallback(
+                PhGetPluginCallback(PluginInstance, PluginCallbackLoad),
+                LoadCallback,
+                NULL,
+                &PluginLoadCallbackRegistration
+                );
+            PhRegisterCallback(
+                PhGetPluginCallback(PluginInstance, PluginCallbackShowOptions),
+                ShowOptionsCallback,
+                NULL,
+                &PluginShowOptionsCallbackRegistration
+                );
+            PhRegisterCallback(
+                PhGetPluginCallback(PluginInstance, PluginCallbackMenuItem),
+                MenuItemCallback,
+                NULL,
+                &PluginMenuItemCallbackRegistration
+                );
+            PhRegisterCallback(
+                PhGetGeneralCallback(GeneralCallbackMainMenuInitializing),
+                MainMenuInitializingCallback,
+                NULL,
+                &MainMenuInitializingCallbackRegistration
+                );
+
+            PhAddSettings(settings, ARRAYSIZE(settings));
+        }
+        break;
+    }
+
+    return TRUE;
+}
---
+++ ObjectManagerPlugin/main.h
@@ -0,0 +1,55 @@
+#ifndef _WINOBJ_H
+#define _WINOBJ_H
+
+#include <phdk.h>
+#include <workqueue.h>
+#include "resource.h"
+
+#define WINOBJ_MENU_ITEM 1000
+#define PLUGIN_NAME L"dmex.ObjectManagerPlugin"
+#define SETTING_NAME_WINDOW_POSITION (PLUGIN_NAME L".WindowPosition")
+#define SETTING_NAME_WINDOW_SIZE (PLUGIN_NAME L".WindowSize")
+#define SETTING_NAME_COLUMNS (PLUGIN_NAME L".WindowColumns")
+
+extern PPH_PLUGIN PluginInstance;
+
+typedef struct _OBJECT_ENTRY
+{
+    PPH_STRING Name;
+    PPH_STRING TypeName;
+} OBJECT_ENTRY, *POBJECT_ENTRY;
+
+typedef struct _OBJ_CONTEXT
+{
+    HWND ListViewHandle;
+    HWND TreeViewHandle;
+    PH_LAYOUT_MANAGER LayoutManager;
+
+    HTREEITEM RootTreeObject;
+    HTREEITEM SelectedTreeItem;
+
+    HIMAGELIST TreeImageList;
+    HIMAGELIST ListImageList;
+} OBJ_CONTEXT, *POBJ_CONTEXT;
+
+typedef struct _DIRECTORY_ENUM_CONTEXT
+{
+    HWND TreeViewHandle;
+    HTREEITEM RootTreeItem;
+    PH_STRINGREF DirectoryPath;
+} DIRECTORY_ENUM_CONTEXT, *PDIRECTORY_ENUM_CONTEXT;
+
+NTSTATUS EnumDirectoryObjects(
+    _In_ HWND TreeViewHandle,
+    _In_ HTREEITEM RootTreeItem,
+    _In_ PH_STRINGREF DirectoryPath
+    );
+
+INT_PTR CALLBACK WinObjDlgProc(
+    _In_ HWND hwndDlg,
+    _In_ UINT uMsg,
+    _In_ WPARAM wParam,
+    _In_ LPARAM lParam
+    );
+
+#endif
