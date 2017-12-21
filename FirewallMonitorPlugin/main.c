f69fb92357d7bf3dbd13599f99710789f54ae9ee
diversenok <30962924+diversenok@users.noreply.github.com>
2017-12-21 23:53:55 +0300

    PhFindEMenuItem do not need ampersands

--- FirewallMonitorPlugin/main.c
+++ FirewallMonitorPlugin/main.c
@@ -93,7 +93,7 @@ VOID NTAPI MainMenuInitializingCallback(
     if (menuInfo->u.MainMenu.SubMenuIndex != PH_MENU_ITEM_LOCATION_TOOLS)
         return;
 
-    if (!(systemMenu = PhFindEMenuItem(menuInfo->Menu, 0, L"&System", 0)))
+    if (!(systemMenu = PhFindEMenuItem(menuInfo->Menu, 0, L"System", 0)))
     {
         PhInsertEMenuItem(menuInfo->Menu, PhPluginCreateEMenuItem(PluginInstance, PH_EMENU_SEPARATOR, 0, L"", NULL), -1);
         PhInsertEMenuItem(menuInfo->Menu, systemMenu = PhPluginCreateEMenuItem(PluginInstance, 0, 0, L"&System", NULL), -1);
